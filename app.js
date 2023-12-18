9ec12ac68ccb4eee473db34988abd6d0d2ce322e
Garry <personarpg7501@gmail.com>
2023-12-18 18:09:29 +0900

    check : 만들어진 테이블을 눈으로 확인하기 위한 작업

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-// mysql2 패키지를 불러옴
 const mysql = require('mysql2');
 
-// MariaDB 연결 설정 객체 생성
 const connection = mysql.createConnection({
-    // MariaDB 서버의 호스트 주소 (보통 localhost 또는 127.0.0.1)
     host: 'localhost',
-    // MariaDB에 접속할 사용자 이름
-    user: 'root', // 여기서 'root'을 실제 MariaDB 사용자 이름으로 변경
-    // MariaDB에 접속할 사용자 비밀번호
-    password: '7501', // 여기서 '7501'을 실제 MariaDB 사용자 비밀번호로 변경
-    // 사용할 데이터베이스 이름
-    database: 'admin', // 여기서 'your_database'를 실제 사용할 데이터베이스 이름으로 변경
+    user: 'your_username',
+    password: '7501',
+    database: 'admin',
 });
 
-// 연결 테스트
 connection.connect((err) => {
-    // 연결 중 에러가 발생한 경우
     if (err) {
         console.error('Error connecting to MariaDB:', err);
         return;
     }
-    // 연결이 성공하면 콘솔에 메시지 출력
     console.log('데이터베이스 연결 성공!');
-});
 
-// 연결 종료
-connection.end();
+    // 테이블 조회 쿼리
+    const query = 'SELECT * FROM users';
+    connection.query(query, (err, results) => {
+        if (err) {
+            console.error('Error querying database:', err);
+            return;
+        }
+
+        // 조회 결과 출력
+        console.log('users 테이블의 데이터:');
+        console.table(results);
+
+        // 연결 종료
+        connection.end();
+    });
+});
