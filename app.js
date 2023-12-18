7ebf62de809e9acc74a3ab52df6705d834586064
Garry <personarpg7501@gmail.com>
2023-12-18 17:29:22 +0900

    check : 데이터베이스 연결 테스트 성공

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const connection = mysql.createConnection({
     // MariaDB 서버의 호스트 주소 (보통 localhost 또는 127.0.0.1)
     host: 'localhost',
     // MariaDB에 접속할 사용자 이름
-    user: 'your_username', // 여기서 'your_username'을 실제 MariaDB 사용자 이름으로 변경
+    user: 'root', // 여기서 'root'을 실제 MariaDB 사용자 이름으로 변경
     // MariaDB에 접속할 사용자 비밀번호
     password: '7501', // 여기서 '7501'을 실제 MariaDB 사용자 비밀번호로 변경
     // 사용할 데이터베이스 이름
-    database: 'your_database', // 여기서 'your_database'를 실제 사용할 데이터베이스 이름으로 변경
+    database: 'admin', // 여기서 'your_database'를 실제 사용할 데이터베이스 이름으로 변경
 });
 
 // 연결 테스트
